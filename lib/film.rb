673ca5fa4b50576da86ea0f6c0b9b122aef2c5d6
Ilja Marinin <ilja.marinin@gmail.com>
2012-04-06 15:06:43 +0300

    Film, FilmCollection creation

---
+++ lib/film.rb
@@ -0,0 +1,3 @@
+class Film
+  # To change this template use File | Settings | File Templates.
+end
---
+++ lib/film_collection.rb
@@ -0,0 +1,3 @@
+class FilmCollection
+  # To change this template use File | Settings | File Templates.
+end
