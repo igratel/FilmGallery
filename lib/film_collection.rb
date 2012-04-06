134bab233fb7b67bcacaccc79ca4d84a958822f9
Ilja Marinin <ilja.marinin@gmail.com>
2012-04-06 16:16:03 +0300

    FilmCollection now passes the test

--- lib/film_collection.rb
+++ lib/film_collection.rb
@@ -14,4 +14,20 @@ class FilmCollection
   def find (title)
     @films.find { |film| title == film.title }
   end
+
+  def change_title (old_title, new_title)
+    if find(old_title) != nil
+      if find(new_title) == nil
+        film = Film.new(new_title)
+        old_film_index = @films.find_index(find(old_title))
+        @films[old_film_index] = film
+        find(new_title)
+      else
+        find(old_title)
+      end
+    else
+      nil
+    end
+
+  end
 end
