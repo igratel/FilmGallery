40bd4b04cede24cebbe3faa8dcbe726deaa43bbd
Ilja Marinin <ilja.marinin@gmail.com>
2012-04-06 15:02:15 +0300

    first commit

---
+++ spec/film_collection_spec.rb
@@ -0,0 +1,10 @@
+require "rspec"
+
+describe "Film Collection" do
+
+  it "should add new film to the end of collection" do
+
+    #To change this template use File | Settings | File Templates.
+    true.should == false
+  end
+end
