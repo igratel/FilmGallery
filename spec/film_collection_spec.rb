8b55d6b5c53b9acef017b9fef3e1078fd5b1a1aa
Ilja Marinin <ilja.marinin@gmail.com>
2012-04-06 16:01:23 +0300

    FilmCollection test updated

--- spec/film_collection_spec.rb
+++ spec/film_collection_spec.rb
@@ -29,4 +29,13 @@ describe "Film Collection" do
     film_collection.find("some other title").title.should == "some other title"
     film_collection.find("some not existing title").should == nil
   end
+
+  it "should change title of film if it does not violate unique constraint" do
+    film_collection = FilmCollection.new
+    film_collection.add(Film.new("some title"))
+    film_collection.change_title("some title", "some new title").title.should == "some new title"
+    film_collection.add(Film.new("some other title"))
+    film_collection.change_title("some title", "some other title").title.should_not == "some other title"
+  end
+
 end
